ca5d2a3f22c15056395d3516d246b9ed72c79845
AliAhmadAyoob <aliahamdayoob@gmail.com>
2026-01-15 11:03:07 +0500

    Added content to resume-parser and utils

--- resume_parser.py
+++ resume_parser.py
@@ -1,1 +1,17 @@
+import pdfplumber as pd
+import docx
 
+def text_extracter(file):
+    text = ""
+    if file.endswith('.pdf'):
+        with pd.open(file) as pdf:
+            for page in pdf.pages:
+                if page.extract_text():
+                    text += page.extract_text() +'\n'
+    elif file.endswith('.docx'):
+        doc = docx.Document(file)
+        for para in doc.paragraphs:
+            text += para.text+'\n'
+    else:
+        raise ValueError("Unsopported File Format")
+    return text
--- utils.py
+++ utils.py
@@ -1,1 +1,20 @@
+import re
+import nltk
+from nltk.corpus import stopwords
 
+nltk.download('stopwords')
+words_to_retain = {"c", "r", "ai", "ml", "sql", "js", "html", "css", "c++", "c#", "django", "react"}
+unwanted= {"email", "website", "skills", "phone", "linkedin"}
+stop_words = set(stopwords.words('english'))
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r'\S+@\S+','',text)
+    text = re.sub(r'http\S+|www.\S+','',text)
+    text = re.sub(r'[^a-z0-9\s+#]',' ',text)
+    words = text.split()
+
+    cleaned_text = ' '.join([w for w in words if (w not in stop_words or w in words_to_retain) and w not in unwanted])
+    cleaned_text = re.sub(r'\s+',' ',cleaned_text).strip()
+
+    return cleaned_text    
