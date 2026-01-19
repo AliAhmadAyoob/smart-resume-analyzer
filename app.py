30c47fa2c3bc323acb6249cb92020d0b67a6b403
AliAhmadAyoob <aliahmadayoob@gmail.com>
2026-01-19 22:12:43 +0500

    Update app.py

--- app.py
+++ app.py
@@ -184,6 +184,7 @@ if uploaded_file:
 
 else:
     st.warning("Please upload a resume in the sidebar to begin.")
-    st.image("https://res.cloudinary.com/dhqxovvve/image/upload/v1709883500/resume_review_illustration.png", width=350)
+    st.image("st.image("https://res.cloudinary.com/dhqxovvve/image/upload/v1709883868/job_app_review.png", width=350)
+
 
 
